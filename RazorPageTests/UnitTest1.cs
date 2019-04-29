11db289eb79f918b46ae5f07f020531e27ba4538
talat.tufekci <TALAT.TUFEKCI@tarnet.com.tr>
2019-04-29 16:49:58 +0300

    added test

--- RazorPageTests/UnitTest1.cs
+++ RazorPageTests/UnitTest1.cs
@@ -1,14 +1,24 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using SampleWebProject;
 using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace RazorPageTests
 {
-    public class UnitTest1
+    public class UnitTest1 : IClassFixture<WebApplicationFactory<Startup>>
     {
-        [Fact]
-        public void Test1()
-        {
+        protected readonly HttpClient _client;
 
+        [Fact]
+        public async Task Test1()
+        {
+            var response =  await _client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("Welcome", stringResponse);
         }
     }
 }
